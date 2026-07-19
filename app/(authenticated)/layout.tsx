6560a9b373a43e02eccf3a92eb594cec04c6351a
amberrrruby <amberrrruby.92@proton.me>
2026-07-19 19:13:39 +0800

    fix(ui): hydration issue; stale user pop-up

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/app/components/Sidebar";
 import { requireUserOrRedirectLogin } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { cookies } from "next/headers";
 
 export default async function AuthenticatedLayout({
   children,
@@ -9,9 +10,14 @@ export default async function AuthenticatedLayout({
 }) {
   const userId = await requireUserOrRedirectLogin();
   const userProfile = await prisma.user.findUnique({ where: { id: userId } });
+  const cookieStore = await cookies();
+  const collapsed = cookieStore.get("sidebar:collapsed")?.value === "true";
+
   return (
     <>
-      <Sidebar user={userProfile!}>{children}</Sidebar>
+      <Sidebar user={userProfile!} defaultCollapsed={collapsed}>
+        {children}
+      </Sidebar>
     </>
   );
 }
